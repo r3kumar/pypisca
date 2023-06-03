8840006ab63b3047c6088f4042f34767c42ea31d
r3mkumar <79623032+r3mkumar@users.noreply.github.com>
2023-06-03 14:49:28 +0530

    download and extract simultaneously using thread

--- download.py
+++ download.py
@@ -3,8 +3,10 @@ import requests
 import tarfile
 import zipfile
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
 
-def download_file(url, local_filename):
+def download_file(url, local_filename, queue):
     # Stream download to handle big files
     r = requests.get(url, stream=True)
     r.raise_for_status()
@@ -12,25 +14,32 @@ def download_file(url, local_filename):
     total_size = int(r.headers.get('content-length', 0))
     block_size = 1024 # 1 KByte
 
-    t=tqdm(total=total_size, unit='iB', unit_scale=True)
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
     with open(local_filename, 'wb') as f:
         for data in r.iter_content(block_size):
             t.update(len(data))
             f.write(data)
     t.close()
-    return local_filename
+    
+    # Put file into the queue for extraction
+    queue.put(local_filename)
 
-def extract_file(filename, path_to_extract):
-    if filename.endswith('.tar.gz'):
-        with tarfile.open(filename, 'r:gz') as tar:
-            for member in tar.getmembers():
-                tar.extract(member, path=path_to_extract)
-                print(f'Extracted: {member.name}')
-    elif filename.endswith('.whl') or filename.endswith('.zip'):
-        with zipfile.ZipFile(filename, 'r') as zip_ref:
-            for member in zip_ref.namelist():
-                zip_ref.extract(member, path_to_extract)
-                print(f'Extracted: {member}')
+def extract_file(queue, path_to_extract):
+    while True:
+        filename = queue.get()
+        if filename is None:
+            break
+        if filename.endswith('.tar.gz'):
+            with tarfile.open(filename, 'r:gz') as tar:
+                for member in tar.getmembers():
+                    tar.extract(member, path=path_to_extract)
+                    print(f'Extracted: {member.name}')
+        elif filename.endswith('.whl') or filename.endswith('.zip'):
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                for member in zip_ref.namelist():
+                    zip_ref.extract(member, path_to_extract)
+                    print(f'Extracted: {member}')
+        queue.task_done()
 
 # Take package name as input
 package_name = input("Enter the package name: ")
@@ -43,16 +52,32 @@ data = response.json()
 package_dir = os.path.join('downloads', package_name)
 os.makedirs(package_dir, exist_ok=True)
 
-# Download each file
+# Prepare list for download tasks
+download_tasks = []
+
+# Prepare each download task
 for version, files in data['releases'].items():
     for file_info in files:
-        # Only download .whl or .tar.gz files
+        # Only handle .whl or .tar.gz files
         if file_info['packagetype'] in ['bdist_wheel', 'sdist']:
             url = file_info['url']
             filename = os.path.join(package_dir, url.split('/')[-1])
-            print(f'Downloading {url} to {filename}')
-            download_file(url, filename)
-            extract_path = os.path.join(package_dir, filename.split('/')[-1].split('.')[0])
-            os.makedirs(extract_path, exist_ok=True)
-            print(f'Extracting {filename} to {extract_path}')
-            extract_file(filename, extract_path)
+            download_tasks.append((url, filename))
+
+queue = Queue()
+
+# Start a thread for extraction
+extractor_thread = threading.Thread(target=extract_file, args=(queue, package_dir))
+extractor_thread.start()
+
+# Run download tasks in a thread pool
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for url, filename in download_tasks:
+        executor.submit(download_file, url, filename, queue)
+
+# Block until all tasks are done
+queue.join()
+
+# Stop the extractor thread
+queue.put(None)
+extractor_thread.join()
