22b3135987f02b375f81b40ed38674aae39af7c1
r3mkumar <79623032+r3mkumar@users.noreply.github.com>
2023-06-03 14:36:02 +0530

    modified code to show download and extraction progress

--- download.py
+++ download.py
@@ -2,23 +2,35 @@ import os
 import requests
 import tarfile
 import zipfile
+from tqdm import tqdm
 
 def download_file(url, local_filename):
     # Stream download to handle big files
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                f.write(chunk)
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+
+    total_size = int(r.headers.get('content-length', 0))
+    block_size = 1024 # 1 KByte
+
+    t=tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open(local_filename, 'wb') as f:
+        for data in r.iter_content(block_size):
+            t.update(len(data))
+            f.write(data)
+    t.close()
     return local_filename
 
 def extract_file(filename, path_to_extract):
     if filename.endswith('.tar.gz'):
         with tarfile.open(filename, 'r:gz') as tar:
-            tar.extractall(path=path_to_extract)
+            for member in tar.getmembers():
+                tar.extract(member, path=path_to_extract)
+                print(f'Extracted: {member.name}')
     elif filename.endswith('.whl') or filename.endswith('.zip'):
         with zipfile.ZipFile(filename, 'r') as zip_ref:
-            zip_ref.extractall(path=path_to_extract)
+            for member in zip_ref.namelist():
+                zip_ref.extract(member, path_to_extract)
+                print(f'Extracted: {member}')
 
 # Take package name as input
 package_name = input("Enter the package name: ")
